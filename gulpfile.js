9f17b4057f6939adf49dc1c6fa84f18b4eec832c
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-05-05 20:10:11 +0200

    Added basic styles with bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
 var browserify = require('gulp-browserify');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('build-js', function() {
     return gulp.src(['./src/assets/dist/js/index.js'])
@@ -17,14 +19,19 @@ gulp.task('build-js', function() {
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src(['./src/assets/dist/css/app.css'])
-    .pipe(cleanCSS({}))
-    .pipe(rename("app.min.css"))
-    .pipe(gulp.dest('./web/assets/dist/css'));
+    return gulp.src(
+            [
+                './node_modules/bootstrap/dist/css/bootstrap.css',
+                './node_modules/bootstrap/dist/css/bootstrap-theme.css',
+                './src/assets/dist/css/app.css'
+            ])
+        .pipe(concat('./src/assets/dist/css/bundle.css'))
+        .pipe(rename("app.min.css"))
+        .pipe(gulp.dest('./web/assets/dist/css'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(['./web/assets/dist/js/index.js', './web/assets/dist/css/app.css'], ['prod']);
 });
 
-gulp.task('default', ['build-js','minify-css'], function() {});
+gulp.task('default', ['build-js', 'minify-css'], function() {});
