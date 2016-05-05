3c9a6cc48c9110dcfb879a4e38b1c0087a4e8b5a
vicaba <vicaba@me.com>
2016-05-05 21:06:31 +0200

    Most viewed articles counts are now stored in redis

--- src/Performance/Infrastructure/Database/DoctrineArticleRepository.php
+++ src/Performance/Infrastructure/Database/DoctrineArticleRepository.php
@@ -8,7 +8,11 @@ use Performance\Domain\ArticleRepository;
 
 class DoctrineArticleRepository extends EntityRepository implements ArticleRepository
 {
-    public function save(Article $article) {
+
+    const ARTICLE_ID_KEY = "id";
+
+    public function save(Article $article)
+    {
         $this->_em->persist($article);
         $this->_em->flush();
     }
@@ -17,8 +21,26 @@ class DoctrineArticleRepository extends EntityRepository implements ArticleRepos
      * @param $article_id
      * @return null|Article
      */
-    public function findOneById($article_id)
+    public function findOneById($articleId)
     {
-        return parent::findOneById($article_id);
+        return parent::findOneById($articleId);
+    }
+
+    public function getListByIds(array $articleIds)
+    {
+
+        if (empty($articleIds)) return [];
+
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select("a")
+            ->from("a", "Performance\\Domain\\Article")
+            ->where($qb->expr()->eq(self::ARTICLE_ID_KEY, $articleIds[0]));
+
+        for ($i = 1; $i <= 5; $i++) {
+            $qb->orWhere($qb->expr()->eq(self::ARTICLE_ID_KEY, $articleIds[$i]));
+        }
+
+        return $qb->getResult();
+
     }
 }
