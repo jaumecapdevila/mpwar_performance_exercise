ccf576d35d9f82eadb004ffe16648611955631b4
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-05-05 21:10:29 +0200

    Added eTag cache to home controller

--- src/Performance/Controller/HomeController.php
+++ src/Performance/Controller/HomeController.php
@@ -2,24 +2,31 @@
 
 namespace Performance\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Performance\Domain\UseCase\ListArticles;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
     /**
      * @var \Twig_Environment
      */
-	private $template;
+    private $template;
 
-    public function __construct(\Twig_Environment $templating, ListArticles $useCase) {
+    public function __construct(\Twig_Environment $templating, ListArticles $useCase, Request $request)
+    {
         $this->template = $templating;
         $this->useCase = $useCase;
+        $this->request = $request;
     }
 
     public function get()
     {
         $articles = $this->useCase->execute();
-        return new Response($this->template->render('home.twig', ['articles' => $articles]));
+        $response = new Response($this->template->render('home.twig', ['articles' => $articles]));
+        $response->setETag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+        return $response;
     }
 }
