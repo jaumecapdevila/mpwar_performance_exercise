a7e430ef39bb5fb3bccb0112f729e7b935a06572
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-05-14 17:39:34 +0200

    Implemented Etag cache in the articles controller

--- src/Performance/Controller/ArticleController.php
+++ src/Performance/Controller/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace Performance\Controller;
 
+use Performance\Domain\UseCase\ReadArticle;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Performance\Domain\UseCase\ReadArticle;
 
 class ArticleController
 {
@@ -24,14 +25,16 @@ class ArticleController
      */
     private $session;
 
-    public function __construct(\Twig_Environment $templating, ReadArticle $useCase, SessionInterface $session) {
+    public function __construct(\Twig_Environment $templating, ReadArticle $useCase, SessionInterface $session)
+    {
         $this->template = $templating;
         $this->useCase = $useCase;
         $this->session = $session;
     }
 
-    public function get($articleId)
+    public function get($articleId, Request $request)
     {
+        $this->fixETag($request);
         $authorId = $this->session->get('authorId');
 
         $article = $this->useCase->execute($articleId, $authorId);
@@ -40,6 +43,17 @@ class ArticleController
             throw new HttpException(404, "Article $articleId does not exist.");
         }
 
-        return new Response($this->template->render('article.twig', ['article' => $article]));
+        $response = new Response($this->template->render('article.twig', ['article' => $article]));
+        $response->setETag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+        return $response;
+    }
+
+    private function fixETag(Request $request)
+    {
+        $oldETag = $request->headers->get('if_none_match');
+        $etagWithoutGzip = str_replace('-gzip"', '"', $oldETag);
+        $request->headers->set('if_none_match', $etagWithoutGzip);
     }
 }
