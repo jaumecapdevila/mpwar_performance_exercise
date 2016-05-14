b648da6051c9de8c7f628e3a23f40277cce20809
vicaba <vicaba@me.com>
2016-05-14 17:15:47 +0200

    Added Most Viewed added articles by user functionality

--- src/Performance/Controller/HomeController.php
+++ src/Performance/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace Performance\Controller;
 use Performance\Domain\UseCase\ListFiveMostViewedArticles;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HomeController
 {
@@ -13,16 +14,34 @@ class HomeController
      */
     private $template;
 
+    /**
+     * @var ListFiveMostViewedArticles
+     */
+    private $useCase;
 
-    public function __construct(\Twig_Environment $templating, ListFiveMostViewedArticles $useCase) {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(\Twig_Environment $templating, ListFiveMostViewedArticles $useCase, SessionInterface $session) {
         $this->template = $templating;
         $this->useCase = $useCase;
+        $this->session = $session;
     }
 
     public function get(Request $request)
     {
+
+        $userArticles = [];
         $articles = $this->useCase->execute();
-        $response = new Response($this->template->render('home.twig', ['articles' => $articles]));
+
+        if (!is_null($this->session)) {
+            $authorId = $this->session->get('authorId');
+            $userArticles = $this->useCase->execute($authorId);
+        }
+
+        $response = new Response($this->template->render('home.twig', ['articles' => $articles, 'userArticles' => $userArticles]));
         $response->setETag(md5($response->getContent()));
         $response->setPublic();
         $response->isNotModified($request);
